723db8c5277862260a963a406a3140a139bc13e7
Dave Duck <dev.programsofdaveduck@gmail.com>
2019-08-23 17:51:20 -0500

    Consistent tabs and spaces

--- src/raytraceCpp.cpp
+++ src/raytraceCpp.cpp
@@ -9,13 +9,13 @@ const float FOV = 0.698;
 
 class Vec3 {
 public:
-	float x, y, z;
-	Vec3();
-	Vec3(float x, float y, float z);
+    float x, y, z;
+    Vec3();
+    Vec3(float x, float y, float z);
 
-	Vec3 normalize() const;
-	Vec3 operator+(const Vec3 &other) const;
-	Vec3 operator*(const float &other) const;
+    Vec3 normalize() const;
+    Vec3 operator+(const Vec3 &other) const;
+    Vec3 operator*(const float &other) const;
 };
 
 Vec3::Vec3(): x(0), y(0), z(0) {}
@@ -25,25 +25,25 @@ Vec3 Vec3::normalize() const {
     return Vec3(x/mag, y/mag, z/mag);
 }
 Vec3 Vec3::operator+(const Vec3 &other) const {
-	return Vec3(x+other.x, y+other.y, z+other.z);
+    return Vec3(x+other.x, y+other.y, z+other.z);
 }
 Vec3 Vec3::operator*(const float &other) const {
-	return Vec3(x*other, y*other, z*other);
+    return Vec3(x*other, y*other, z*other);
 }
 
 float mod(const float &x, const float &n) {
-	return x - floor(x/n) * n;
+    return x - floor(x/n) * n;
 }
 
 Vec3 mod(const Vec3 &x, const float &n) {
-	return Vec3(mod(x.x, n), mod(x.y,n), mod(x.z,n));
+    return Vec3(mod(x.x, n), mod(x.y,n), mod(x.z,n));
 }
 
 float distance(const Vec3 &a, const Vec3 &b) {
-	float diffX = a.x - b.x;
-	float diffY = a.y - b.y;
-	float diffZ = a.z - b.z;
-	return sqrt(diffX*diffX + diffY*diffY + diffZ*diffZ);
+    float diffX = a.x - b.x;
+    float diffY = a.y - b.y;
+    float diffZ = a.z - b.z;
+    return sqrt(diffX*diffX + diffY*diffY + diffZ*diffZ);
 }
 
 float fractalDistance(const Vec3 &x) {
@@ -56,7 +56,7 @@ float lightIntensity(const Vec3 &x, const Vec3 &light, const Vec3 &camera) {
 }
 
 bool raytrace(const Vec3 &startPos, const Vec3 &direction, Vec3 *outVec) {
-	Vec3 rayPos = startPos;
+    Vec3 rayPos = startPos;
     float d = 1.0f;
     while(d > 0.05) {
         rayPos = rayPos + direction * d;
@@ -64,9 +64,9 @@ bool raytrace(const Vec3 &startPos, const Vec3 &direction, Vec3 *outVec) {
 
         if(distance(rayPos, startPos) > 30.0f) {
             return false;
-		}
+        }
     }
-	*outVec = rayPos;
+    *outVec = rayPos;
     return true;
 }
 
@@ -75,47 +75,47 @@ Vec3 fragmentShader(const Vec3 &cameraPos, const Vec3 &lightPos, int x, int y) {
     float angleY = FOV * (2.0f*((float)y/HEIGHT) - 1.0f);
     Vec3 direction = Vec3(tan(angleX), tan(angleY), 1.0f).normalize();
 
-	Vec3 rayResult;
+    Vec3 rayResult;
     bool found = raytrace(cameraPos, direction, &rayResult);
     if(!found) {
-		return Vec3(0.0f, 0.0f, 0.0f);
-	}
+        return Vec3(0.0f, 0.0f, 0.0f);
+    }
     float shade = lightIntensity(rayResult, lightPos, cameraPos);
-	return Vec3(shade, 0, 0);
+    return Vec3(shade, 0, 0);
 }
 
 void drawScreen(SDL_Surface *screen) {
-	Vec3 cameraPos(0.0f, 0.0f, -3.0f);
-	Vec3 lightPos(0.0f, 1.0f, -0.5f);
-	SDL_Rect rect;
-	rect.w = 1;
-	rect.h = 1;
+    Vec3 cameraPos(0.0f, 0.0f, -3.0f);
+    Vec3 lightPos(0.0f, 1.0f, -0.5f);
+    SDL_Rect rect;
+    rect.w = 1;
+    rect.h = 1;
 
-	for(int x = 0; x < WIDTH; x++) {
-		rect.x = x;
-		for(int y = 0; y < HEIGHT; y++) {
-			rect.y = y;
-			Vec3 color = fragmentShader(cameraPos, lightPos, x, y) * 255.0f;
-			SDL_FillRect(screen, &rect, SDL_MapRGB(screen->format, color.x, color.y, color.z));
-		}
-	}
-	SDL_Flip(screen);
+    for(int x = 0; x < WIDTH; x++) {
+        rect.x = x;
+        for(int y = 0; y < HEIGHT; y++) {
+            rect.y = y;
+            Vec3 color = fragmentShader(cameraPos, lightPos, x, y) * 255.0f;
+            SDL_FillRect(screen, &rect, SDL_MapRGB(screen->format, color.x, color.y, color.z));
+        }
+    }
+    SDL_Flip(screen);
 }
 
 void waitForKey() {
-	SDL_Event event;
-	while(event.type != SDL_KEYDOWN) {
-		SDL_WaitEvent(&event);
-	}
+    SDL_Event event;
+    while(event.type != SDL_KEYDOWN) {
+        SDL_WaitEvent(&event);
+    }
 }
 
 int main(void) {
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Surface *screen = SDL_SetVideoMode(WIDTH, HEIGHT, has_colors ? 16 : 8, SDL_SWSURFACE);
-	drawScreen(screen);
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Surface *screen = SDL_SetVideoMode(WIDTH, HEIGHT, has_colors ? 16 : 8, SDL_SWSURFACE);
+    drawScreen(screen);
 
-	waitForKey();
+    waitForKey();
 
-	SDL_Quit();
-	return 0;
+    SDL_Quit();
+    return 0;
 }
