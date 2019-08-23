2cb5ce0d9d9ba84605f5fc98919da8e001fc09cb
Dave Duck <dev.programsofdaveduck@gmail.com>
2019-08-23 17:44:31 -0500

    Lua implementation closer to cpp

--- src/raytraceLua.lua
+++ src/raytraceLua.lua
@@ -2,7 +2,6 @@ local screen = platform.window
 local w = screen:width()
 local h = screen:height()
 local FOV = 0.698
-local cameraPos = {0, 0, -3}
 
 function distance(x, y)
     local diffX = x[1]-y[1]
@@ -29,9 +28,12 @@ function norm(x)
     return {x[1]/mag, x[2]/mag, x[3]/mag}
 end
 
-function raytrace(startPos, direction)
+function raytrace(startPos, direction, rayPos)
+    rayPos[1] = startPos[1]
+    rayPos[2] = startPos[2]
+    rayPos[3] = startPos[3]
+
     local d = 1
-    local rayPos = {startPos[1], startPos[2], startPos[3]}
     while(d > 0.05) do
         rayPos[1] = rayPos[1] + direction[1] * d
         rayPos[2] = rayPos[2] + direction[2] * d
@@ -39,29 +41,32 @@ function raytrace(startPos, direction)
         d = fractalDistance(rayPos)
 
         if(distance(rayPos, startPos) > 30) then
-            return {rayPos, false}
+            return false
         end
     end
-    return {rayPos, true}
+    return true
 end
 
-function FragmentShader(cameraPos, x, y)
+function FragmentShader(cameraPos, lightPos, x, y)
     local angleX = FOV * (2*x/w - 1)
     local angleY = FOV * (2*y/h - 1)
     local direction = norm({math.tan(angleX), math.tan(angleY), 1})
 
-    local result = raytrace(cameraPos, direction)
-    if(not result[2]) then
+    local rayResult = {0, 0, 0}
+    local found = raytrace(cameraPos, direction, rayResult)
+    if(not found) then
         return {0, 0, 0}
     end
-    local shade = lightIntensity(result[1], {0, 1, -0.5}, cameraPos)
+    local shade = lightIntensity(rayResult, lightPos, cameraPos)
     return {shade, 0, 0}
 end
 
 function Draw(gc)
+    local cameraPos = {0, 0, -3}
+    local lightPos = {0, 1, -0.5}
     for x=1, w do
         for y=1, h do
-            color = FragmentShader(cameraPos, x, y)
+            color = FragmentShader(cameraPos, lightPos, x, y)
             gc:setColorRGB(math.min(color[1]*255, 255), math.min(color[2]*255), math.min(color[3]*255, 255))
             gc:fillRect(x, y, 1, 1)
         end
